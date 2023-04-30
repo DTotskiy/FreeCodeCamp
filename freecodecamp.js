142480f2ad0c353ade5ebf73b3be1b74f8e82f2d
DTotskiy <132058678+DTotskiy@users.noreply.github.com>
2023-04-30 13:55:14 +0300

    Add files via upload

---
+++ freecodecamp.js
@@ -0,0 +1,32 @@
+//1. Comment Your JavaScript Code:
+
+//This is first comment
+/*This is
+ second comment */
+
+//2. Declare JavaScript Variables:
+
+var myName;
+myName = '2. Dmitriy';
+console.log (myName);
+
+//3. Storing Values with the Assignment Operator:
+
+var a;
+a = 7;
+
+//4. Assigning the Value of One Variable to Another:
+
+var a;
+a = 7;
+var b;
+b = a;
+
+//5. Initializing Variables with the Assignment Operator:
+
+var a = 9;
+
+//6. Declare String Variables:
+
+var myFirstName = "Dmitriy";
+var myLastName = "Totskiy";
