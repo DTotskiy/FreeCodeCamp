8f4778cae1d44ebcbfc7940d56538db6795a8aae
DTotskiy <132058678+DTotskiy@users.noreply.github.com>
2023-04-30 16:04:26 +0500

    Update freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -30,3 +30,14 @@ var a = 9;
 
 var myFirstName = "Dmitriy";
 var myLastName = "Totskiy";
+
+//7. Understanding Uninitialized Variables:
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
