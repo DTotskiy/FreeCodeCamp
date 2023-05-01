52a92b08030caa35a83fa13eba7b586221ab9e0f
DTotskiy <132058678+DTotskiy@users.noreply.github.com>
2023-05-01 14:06:42 +0500

    25 js freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -40,4 +40,126 @@ a = a + 1;
 b = b + 5;
 c = c + " String!";
 
+//8. Understanding Case Sensitivity in Variables:
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//9. Explore Differences Between the var and let Keywords:
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//10. Declare a Read-Only Variable with the const Keyword:
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//11. Add Two Numbers with JavaScript::
+
+const sum = 10 + 10;
+
+//12. Subtract One Number from Another with JavaScript:
+
+const difference = 45 - 33;
+
+//13. Multiply Two Numbers with JavaScript:
+
+const product = 8 * 10;
+
+//14. Divide One Number by Another with JavaScript:
+
+const quotient = 66 / 33;
+
+//15. Increment a Number with JavaScript:
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//16. Decrement a Number with JavaScript:
+
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+//17. Create Decimal Numbers with JavaScript:
+
+const ourDecimal = 5.7;
+// Only change code below this line
+let myDecimal = 1.2;
+
+//18. Multiply Two Decimals with JavaScript:
+
+const product = 2.0 * 2.5;
+
+//19. Divide One Decimal by Another with JavaScript:
+
+const quotient = 4.4 / 2.0; // Change this line
+
+//20. Finding a Remainder in JavaScript:
+
+const remainder = 11 % 3;
+
+//21. Compound Assignment With Augmented Addition:
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//22. Compound Assignment With Augmented Subtraction:
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//23. Compound Assignment With Augmented Multiplication:
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//24. Compound Assignment With Augmented Division:
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//25. Escaping Literal Quotes in Strings:
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//26.
+
 
