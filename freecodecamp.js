97e044386d396c56925af592f26e1398f2a01de4
DTotskiy <132058678+DTotskiy@users.noreply.github.com>
2023-05-02 16:41:26 +0500

    30 freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -160,6 +160,29 @@ c /= 11;
 
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
-//26.
+//26.Quoting Strings with Single Quotes:
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//27. Escape Sequences in Strings:
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//28. Concatenating Strings with Plus Operator:
+
+const myStr = "This is the start." + " This is the end."; // Change this line
+
+//29. Concatenating Strings with the Plus Equals Operator:
+
+let myStr = "This is the first sentence.";
+myStr += " This is the second sentence.";
+
+//30. Constructing Strings with Variables:
+
+// Only change code below this line
+const myName = "Dmitriy ";
+const myStr = "My name is " + myName + "and I am well!";
+
+
 
 
